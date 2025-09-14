aabfb0c0aac2a1dbfc6577a153d40900140e694f
Radiant Forest <yaarkoren@mail.tau.ac.il>
2025-09-14 21:21:02 +0300

    fill board functions

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -68,6 +68,7 @@ public class Grid : MonoBehaviour {
         pieces = new GamePiece[xDim, yDim];
         for (int x = 0; x < xDim; x++) { //rows
             for (int y = 0; y < yDim; y++) { //cols
+                //Debug.Log("inside loop, x: " + x + ", y: " + y);
                 SpwanNewPiece(x, y, PieceType.EMPTY); //init as empty
                 //set the position
                 //if (pieces[x,y].IsMovable()) {
@@ -84,6 +85,8 @@ public class Grid : MonoBehaviour {
                 //}
             }
         }
+
+        Fill();
     }
 
 
@@ -127,6 +130,71 @@ public class Grid : MonoBehaviour {
         return pieces[x, y];    
 
     }
+
+    //calls FillStep untill the board is filled
+    public void Fill()
+    {
+        while (FillStep()) {}
+    }
+
+    //moves each piece only one step
+    //return TRUE if any pieces were moved
+    public bool FillStep() {
+        bool movePiece = false;
+        //loop through all cols in reverse order, bottom to top
+        //we ignore the last one cuz we are looking for pieces we can move down, and the lase one (dim-1) - we can't
+        for (int y = yDim-2; y >=0; y--)
+        {
+            for (int x = 0; x<xDim; x++)
+            {
+                Debug.Log("inside FillStep loop, x: " + x + ", y: " + y);
+                GamePiece piece = pieces[x, y];
+
+                //if it's not movable - we can't move it down to fill the empty space, so we can just ignore it
+                if (piece.IsMovable())
+                {
+                    GamePiece pieceBelow = pieces[x, y + 1];
+                    if (pieceBelow.Type == PieceType.EMPTY)
+                    {
+                        piece.MovableComponent.Move(x, y + 1);
+                        pieces[x, y + 1] = piece;
+                        SpwanNewPiece(x, y, PieceType.EMPTY); //actually we are swapping a movable piece with an empty piece below it
+                        movePiece = true;
+
+                    }
+
+                }
+
+            }
+        }
+
+        //handle top row
+        //the top row is a special case, since empty spaces there will be filled by new pieces created (and not swapping exising movable objects)
+        //loop throuh all the cells in the top row
+        for (int x = 0; x < xDim; x++)
+        {
+            Debug.Log("inside FillStep loop top row, x: " + x + ", y: 0");
+            GamePiece pieceBelow = pieces[x, 0];
+            if (pieceBelow.Type == PieceType.EMPTY)
+            {
+                //we don't use the SpwanNewPiece() func cuz of the -1 thing
+                GameObject newPiece = (GameObject)Instantiate(
+                     piecePrefabDict[PieceType.NORMAL],
+                     GetWorldPos(x, -1), //create in the "non-existing" row, above the top row
+                     Quaternion.identity);
+                newPiece.transform.parent = transform;  
+
+                pieces[x, 0] = newPiece.GetComponent<GamePiece>();
+                pieces[x, 0].Init(x, -1, this, PieceType.NORMAL); //we set it for -1 and not 0, for animation
+                pieces[x, 0].MovableComponent.Move(x, 0);
+                pieces[x, 0].ColorComponent.SetColor( (ColorPiece.ColorType)Random.Range(0, pieces[x,0].ColorComponent.ColorsNum) );
+                movePiece = true;
+
+            }
+        }
+        return movePiece;
+    }
+
 
 }
 
